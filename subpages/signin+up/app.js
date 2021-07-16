e1349b4a5f2dba7efd159ad55bc6358528f19424
Leandro Luna <leandro.j.luna@gmail.com>
2021-07-16 16:41:20 -0300

    login_main.updates

--- subpages/signin+up/app.js
+++ subpages/signin+up/app.js
@@ -1,3 +1,4 @@
+// Transição entre Login/Signup 
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
@@ -9,3 +10,18 @@ sign_up_btn.addEventListener('click', () => {
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 });
+
+// Validar senha
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirmar_senha");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Senhas divergentes");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
